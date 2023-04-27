0b7183454d97f4e5a0f4e170ea0db7ec37fc8043
bhargav-robosoft <bhargavudupa.robosoft@gmail.com>
2023-04-27 17:52:54 +0530

    Commits

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NgxEchartsModule } from 'ngx-echarts';
+import { NgChartsModule } from 'ng2-charts';
 import { LineEchartComponent } from './line-echart/line-echart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HomeComponent, LineEchartComponent],
+  declarations: [HomeComponent, LineEchartComponent, PieChartComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     DashboardRoutingModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
     }),
+    NgChartsModule,
   ],
 })
 export class DashboardModule {}
---
+++ src/app/dashboard/pie-chart/pie-chart.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Chart, ChartData } from "chart.js";
+
+@Component({
+  selector: "app-pie-chart",
+  templateUrl: "./pie-chart.component.html",
+  styleUrls: ["./pie-chart.component.css"],
+})
+export class PieChartComponent implements OnInit {
+  chartForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.chartForm = new FormGroup({
+      portfolio: new FormArray([]),
+    });
+  }
+
+  pieChartData: ChartData | null = null;
+
+  pieChartOptions = {
+    responsive: false,
+  };
+
+  doughnutChartOptions = {
+    responsive: false,
+  };
+
+  onAddCompany(): void {
+    const group = new FormGroup({
+      company: new FormControl(null, Validators.required),
+      amount: new FormControl(null, Validators.required),
+    });
+
+    (<FormArray>this.chartForm.get("portfolio")).push(group);
+  }
+
+  getControls() {
+    return (<FormArray>this.chartForm.get("portfolio")).controls;
+  }
+
+  deleteGroup(index: number) {
+    (<FormArray>this.chartForm.get("portfolio")).removeAt(index);
+  }
+
+  onSubmit() {
+    let amounts: number[] = [];
+    let companies: string[] = [];
+    let totalAmount = 0;
+
+    for (let i = 0; i < this.chartForm.value["portfolio"].length; i++) {
+      let amount: number = this.chartForm.value["portfolio"][i]["amount"];
+      totalAmount += amount;
+    }
+
+    for (let i = 0; i < this.chartForm.value["portfolio"].length; i++) {
+      let amount: number = this.chartForm.value["portfolio"][i]["amount"];
+      let company: string = this.chartForm.value["portfolio"][i]["company"];
+      amounts.push(+((amount / totalAmount) * 100).toFixed(2));
+      companies.push(company);
+    }
+
+    this.setPieChartData(companies, amounts);
+  }
+
+  setPieChartData(labels: string[], data: number[]) {
+    if (labels.length === 0) {
+      this.pieChartData = null;
+      return;
+    }
+
+    this.pieChartData = {
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+          label: "Portfolio Percent",
+          backgroundColor: [
+            "rgba(255, 0, 25, 0.7)",
+            "rgba(0, 255, 25, 0.7)",
+            "rgba(0, 25, 255, 0.7)",
+            "rgba(67, 25, 255, 0.7)",
+            "rgba(67, 25, 78, 0.7)",
+            "rgba(167, 125, 78, 0.7)",
+            "rgba(67, 255, 178, 0.7)",
+          ],
+        },
+      ],
+    };
+  }
+}
